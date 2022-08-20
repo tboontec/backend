0ab0caebd5db6cf28be933bc82d7fc9ea7ce06ea
bryan <tboontec@gmail.com>
2022-08-20 22:10:09 +0800

    update

--- customer.js
+++ customer.js
@@ -5,36 +5,10 @@ const express = require("express");
 // Allows us to define a mapping from the URI to a function
 router = express.Router();
 
-// can be used to define a GET API.   URI -> CB function.
-router.get("/customer/all", (request, response) => {
-  database.connection.all("select * from customer", (errors, results) => {
-    if (errors) {
-      response.status(500).send("Some error occurred");
-    } else {
-      response.status(200).send(results);
-    }
-  });
-});
-//Note: use query instead of all for MySQL - database.connection.query("select * from customer"
-
-// defines an API which takes id in the request and return the record in response
-router.get("/customer/id", (request, response) => {
-  database.connection.all(
-    `select * from customer where customer_id = ${request.query.cid}`,
-    (errors, results) => {
-      if (errors) {
-        response.status(500).send("Some error occurred");
-      } else {
-        response.status(200).send(results);
-      }
-    }
-  );
-});
-
-// a POST API to store the record received in the request
+// API: [Create] a new customer record received in the request
 router.post("/customer/add", (request, response) => {
   database.connection.all(
-    `insert into customer (customer_name, customer_email) values ('${request.body.name}','${request.body.email}')`,
+    `insert into customer (name, email) values ('${request.body.nameA}','${request.body.emailA}')`,
     (errors, results) => {
       if (errors) {
         response.status(500).send("Some error occurred");
@@ -45,10 +19,68 @@ router.post("/customer/add", (request, response) => {
   );
 });
 
-// POST + PUT = Body, GET + DELETE = Query
+// API: [Read] list all customers
+router.get("/customer/all", (request, response) => {
+  database.connection.all("select * from customer", (errors, results) => {
+    if (errors) {
+      response.status(500).send("Some error occurred");
+    } else {
+      response.status(200).send(results);
+    }
+  });
+});
+
+// API: [Read] takes email in the request and return the customer in response
+router.get("/customer/search", (request, response) => {
+  sqlst = `select * from customer where email  = '${request.query.emailS}'`,
+    database.connection.all(
+      sqlst,
+      (errors, results) => {
+        if (errors) {
+          response.status(500).send("Some error occurred" + sqlst);
+        } else {
+          response.status(200).send(results);
+        }
+      }
+    );
+});
+
+// API: [Read] takes email & pwd in the request and return the orders of the customer
+router.post("/customer/login", (request, response) => {
+  sqlst = `select c.name as custName, i.name, o.quantity, o.shipping_date from customer c, item i, shop_order o where c.email = '${request.body.emailL}' and c.pwd = '${request.body.pwdL}'`,
+    database.connection.all(
+      sqlst,
+      (errors, results) => {
+        if (errors) {
+          response.status(500).send("Some error occurred" + sqlst);
+        } else {
+          response.status(200).send(results);
+        }
+      }
+    );
+});
+
+// API: [Update] password for given customer email
+router.put("/customer/update", (request, response) => {
+  sqlstmt = `UPDATE customer SET pwd  = '${request.body.pwdU}'
+    WHERE email = "${request.body.emailU}"`;
+
+  database.connection.all(
+    sqlstmt,
+    (errors, results) => {
+      if (errors) {
+        response.status(500).send("Some error occurred" + sqlstmt);
+      } else {
+        response.status(200).send("Record updated successfully!" + sqlstmt);
+      }
+    }
+  );
+});
+
+// API: [Delete] customer based on email
 router.delete("/customer/delete", (request, response) => {
   database.connection.all(
-    `delete from customer where customer_id  = ${request.query.cid}`,
+    `delete from customer where email  = '${request.query.emailD}'`,
     (errors, results) => {
       if (errors) {
         response.status(500).send("Some error occurred");
--- database.js
+++ database.js
@@ -9,29 +9,3 @@ var connection = new sqlite3.Database('./market.db', (err) => {
 });
 
 module.exports = { connection };
-
-
-//MY SQL 
-/* var properties = {
-  host: "yourdb.mysql.database.azure.com",
-  port: 3306,
-  user: "you@yourdb",
-  password: "FinTechSG2022",
-  database: "market",
-};
-
-var connection = mysql.createConnection(properties);
-
-
-
-connection.connect((errors) => {
-  if (errors) {
-    console.log("Couldn't connect to the MySQL Server. Error: " + errors);
-  } else {
-    console.log("Connected to MySQL successfully!");
-  }
-});   
-
-setInterval(() => {
-  connection.query("select 1");
-}, 60 * 1000); */
